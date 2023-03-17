f9043c66af4fef53601f5d28793da47e8502bc8a
Dusan Zivkovic <dushanzh@gmail.com>
2023-03-17 13:41:06 +0100

    BE path changed

--- src/Utils/globalProps.ts
+++ src/Utils/globalProps.ts
@@ -1,7 +1,9 @@
 const _Global = {
     TOP_RESULTS:10,
     NUMBER_OF_QUESTIONS:10,
-    BE_URL:"http://192.168.1.123:8081"
+    //BE_URL:"http://192.168.1.123:8081",
+    BE_URL:"https://kontehbe.tiaclab.com"
+
 }
 
 export default _Global;
