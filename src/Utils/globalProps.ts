a918d6a5065edf320f61c92df024da45fb1ccfca
Dusan Zivkovic <dushanzh@gmail.com>
2023-03-21 11:42:17 +0100

    glopal be prop redirected

--- src/Utils/globalProps.ts
+++ src/Utils/globalProps.ts
@@ -1,8 +1,8 @@
 const _Global = {
   TOP_RESULTS: 10,
   NUMBER_OF_QUESTIONS: 10,
-  BE_URL: "http://localhost:9090",
-  // BE_URL: "https://kontehbe.tiaclab.com",
+  // BE_URL: "http://localhost:9090",
+  BE_URL: "https://kontehbe.tiaclab.com",
 };
 
 export default _Global;
