2e93ec7ba1544e4212acb1a7243b224d85ae8c1c
Desdenova-FWB <127230648+Desdenova-FWB@users.noreply.github.com>
2023-05-17 16:24:15 +0200

    Update globalProps.ts

--- src/Utils/globalProps.ts
+++ src/Utils/globalProps.ts
@@ -2,7 +2,7 @@ const _Global = {
   TOP_RESULTS: 10,
   NUMBER_OF_QUESTIONS: 10,
   // BE_URL: "http://192.168.179.185:9090",
-  BE_URL: "http://studentsdaybe.tiaclab.com",
+  BE_URL: "http://studentsdaybe.tiaclab.com/",
 };
 
 export default _Global;
