ef4320478f4cbd08780f142ffb292a12d8586a42
nebojsa.zoraja <nebojsa.zoraja@tiacgroup.com>
2023-05-18 14:42:12 +0200

    http

--- src/Utils/globalProps.ts
+++ src/Utils/globalProps.ts
@@ -2,7 +2,7 @@ const _Global = {
   TOP_RESULTS: 10,
   NUMBER_OF_QUESTIONS: 10,
   // BE_URL: "http://localhost:9090",
-  BE_URL: "https://studentsdaybe.tiaclab.com/",
+  BE_URL: "http://studentsdaybe.tiaclab.com/",
 };
 
 export default _Global;
